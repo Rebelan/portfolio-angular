90020289c8762094d85de09cea204e80e522c4c3
Rebelan <abelcm2013@gmail.com>
2026-04-16 08:53:03 +0200

    button material

---
+++ portfolio/src/app/components/shared/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from "@angular/router";
+
+
+export type ButtonVariant =
+  | 'basic'
+  | 'raised'
+  | 'flat'
+  | 'stroked';
+
+
+
+@Component({
+  selector: 'app-button',
+  imports: [MatButtonModule, RouterLink],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+  label = input.required<string>()
+  link = input.required<string>()
+  variant = input<ButtonVariant>('basic');
+}
