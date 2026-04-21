a838e61cf6cf223c9f9e9f3616d88e6ea950039b
Rebelan <abelcm2013@gmail.com>
2026-04-21 08:16:04 +0200

    arreglo dle logout

--- portfolio/src/app/services/auth.service.ts
+++ portfolio/src/app/services/auth.service.ts
@@ -38,6 +38,7 @@ export class AuthService {
   logout(): void {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
+    this.currentUser.set(null);
   }
 
   isLogged(): boolean {
